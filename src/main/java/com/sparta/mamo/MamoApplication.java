c51fcd5c17c52573de8938afd6f46196001c5ec2
jubseok <junseok708@gmail.com>
2024-09-26 17:52:25 +0900

    first commit

---
+++ src/main/java/com/sparta/mamo/MamoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.mamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/mamo/MamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.mamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
